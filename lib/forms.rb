77d757a3c764683285786dbf05245f96694971ed
Tim Lossen <tim@lossen.de>
2013-02-17 21:43:54 +0100

    added help texts

--- lib/forms.rb
+++ lib/forms.rb
@@ -16,25 +16,19 @@ class Signup < Form
   def initialize(params)
     super
     @errors << "Please enter your 3 main Priorities" if priorities.any?(&:empty?)
-    @errors << "Please select one or more Company Age" if company_age.empty?
-    @errors << "Please enter a Job Title" if job_title.empty?
+    @errors << "Please enter a Job Title" if position.empty?
     @errors << "Please select one or more Job Type" if job_type.empty?
     @errors << "Please enter your 3 main traits" if traits.any?(&:empty?)
     @errors << "Please enter your Favorite Language" if favorite_language.empty?
+    @errors << "Please enter your Experience" unless experience =~ /^\d+$/
   end
 
   def priorities
     [1,2,3].map { |i| text_input("priority_#{i}") }
   end
 
-  def company_age
-    %w[baby toddler child teenie adult].map do |option|
-      option if params[:"company_age_#{option}"]
-    end.compact
-  end
-
-  def job_title
-    text_input(:job_title)
+  def position
+    text_input(:position)
   end
 
   def job_type
